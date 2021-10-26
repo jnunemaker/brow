a6ff4bd5143c8b5ca04501dd80a20417a894dfb7
John Nunemaker <nunemaker@gmail.com>
2021-10-25 21:24:56 -0400

    Add docs for all the client options

--- lib/brow/client.rb
+++ lib/brow/client.rb
@@ -18,7 +18,47 @@ module Brow
     # Public: Create a new instance of a client.
     #
     # options - The Hash of options.
+    #   :url - The URL where all batches of data should be transported.
+    #   :test - Should the client be in test mode. If true, all data is stored
+    #           in memory for later verification in a test. If false, things
+    #           work as normal.
     #   :max_queue_size - The maximum number of calls to be remain queued.
+    #   :logger - The Logger to use to log useful information about what is
+    #             going on.
+    #   :queue - The Queue to use to store data until it can be batched up and
+    #            transported to the API.
+    #   :worker - The Worker that will pop items off the queue, batch them up
+    #             and transport them to the API.
+    #   :transport - The Transport to use to transport batches to the API.
+    #   :headers - The Hash of headers to include when transporting batches to
+    #              the API. These could be used for auth or whatever.
+    #   :retries - The Integer number of times the transport should retry a call
+    #              before giving up.
+    #   :read_timeout - The number of seconds to wait when reading data before
+    #                   giving up.
+    #   :open_timeout - The number of seconds to wait when opening a connection
+    #                   to the API.
+    #   :backoff_policy - The BackoffPolicy to use to determine when the next
+    #                     retry should occur when the transport fails to send a
+    #                     batch of data to the API.
+    #   :min_timeout_ms - The minimum number of milliseconds to wait before
+    #                     retrying a failed call to the API.
+    #   :max_timeout_ms - The maximum number of milliseconds to wait before
+    #                     retrying a failed call to the API.
+    #   :multiplier - The value to multily the current interval with for each
+    #                 retry attempt.
+    #   :randomization_factor - The value to use to create a range of jitter
+    #                 around the retry interval.
+    #   :batch - The MessageBatch used to batch up several events to be
+    #            transported in one call to the API.
+    #   :max_size - The maximum number of items a batch can contain before it
+    #               should be transported to the API. Only used if not :batch
+    #               is provided.
+    #   :shutdown_timeout - The number of seconds to wait for the worker thread
+    #                       to join when shutting down.
+    #   :shutdown_automatically - Should the client shutdown automatically or
+    #                             manually. If true, shutdown is automatic. If
+    #                             false, you'll need to handle this on your own.
     #   :on_error - The Proc that handles error calls from the API.
     def initialize(options = {})
       options = Brow::Utils.symbolize_keys(options)
@@ -61,24 +101,24 @@ module Brow
 
     # Public: Enqueues an event to eventually be transported to backend service.
     #
-    # event - The Hash of event data.
+    # data - The Hash of data.
     #
-    # Returns Boolean of whether the item was added to the queue.
-    def push(item)
-      raise ArgumentError, "item must be a Hash" unless item.is_a?(Hash)
+    # Returns Boolean of whether the data was added to the queue.
+    def push(data)
+      raise ArgumentError, "data must be a Hash" unless data.is_a?(Hash)
 
-      item = Brow::Utils.symbolize_keys(item)
-      item = Brow::Utils.isoify_dates(item)
+      data = Brow::Utils.symbolize_keys(data)
+      data = Brow::Utils.isoify_dates(data)
 
       if @test
-        test_queue << item
+        test_queue << data
         return true
       end
 
       ensure_threads_alive
 
       if @queue.length < @max_queue_size
-        @queue << item
+        @queue << data
         true
       else
         @logger.warn("[brow]") { "Queue is full, dropping events. The :max_queue_size configuration parameter can be increased to prevent this from happening." }
